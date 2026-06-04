c9ea7c8c3f4d8f299831b3f7c9f7af71d331bb28
mustafeez-ali <mustafeezalig88@gmail.com>
2026-06-04 15:40:31 +0530

    updated code

--- src/main/java/com/user/mgmt/controller/UserManagementController.java
+++ src/main/java/com/user/mgmt/controller/UserManagementController.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.user.mgmt.entity.Post;
 import com.user.mgmt.entity.User;
 import com.user.mgmt.exception.UserNotFoundException;
 import com.user.mgmt.response.UserResponse;
